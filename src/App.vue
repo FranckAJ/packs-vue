12720f16e36e2ee5e7e65ecd11a9d92ff5f0c81d
Franck Aragão <fjunior.aragao@gmail.com>
2018-05-28 17:14:45 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'PacksComponent',
+
+  components: {
+    HelloWorld
+  }
+}
+</script>
